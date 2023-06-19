c689ed7794bb6ab41e23236aaaee0cfc52989d6c
aadhar.mishra <aadhar.mishra@bookmyshow.com>
2023-06-20 03:02:34 +0530

    Initial commit

---
+++ BooksAPI/BooksController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = _bookRepository.GetAllBooks();
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _bookRepository.GetBookById(id);
+            if (book == null)
+                return NotFound();
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book book)
+        {
+            if (book == null)
+                return BadRequest();
+
+            _bookRepository.AddBook(book);
+            return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book book)
+        {
+            if (book == null || book.Id != id)
+                return BadRequest();
+
+            var existingBook = _bookRepository.GetBookById(id);
+            if (existingBook == null)
+                return NotFound();
+
+            existingBook.Title = book.Title;
+            existingBook.Author = book.Author;
+            _bookRepository.UpdateBook(existingBook);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var existingBook = _bookRepository.GetBookById(id);
+            if (existingBook == null)
+                return NotFound();
+
+            _bookRepository.DeleteBook(id);
+            return NoContent();
+        }
+    }
+}
---
+++ BooksAPI/RedisBookRepository.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
+namespace BooksAPI
+{
+    public class RedisBookRepository: IBookRepository
+    {
+        private readonly IConnectionMultiplexer _connectionMultiplexer;
+        private readonly IDatabase _database;
+
+        public RedisBookRepository(IConnectionMultiplexer connectionMultiplexer)
+        {
+            _connectionMultiplexer = connectionMultiplexer;
+            _database = _connectionMultiplexer.GetDatabase();
+        }
+
+        private RedisKey GetRedisKey(int id) => $"books:{id}";
+
+        public IEnumerable<Book> GetAllBooks()
+        {
+            var redisKeys = _database.Multiplexer.GetServer(_database.Multiplexer.GetEndPoints().First())
+                .Keys(database: _database.Database, pattern: "books:*");
+            var books = _database.StringGet(redisKeys.ToArray()).Select(entry => JsonConvert.DeserializeObject<Book>(entry));
+            return books;
+        }
+
+        public Book GetBookById(int id)
+        {
+            var bookJson = _database.StringGet(GetRedisKey(id));
+            return JsonConvert.DeserializeObject<Book>(bookJson);
+        }
+
+        public void AddBook(Book book)
+        {
+            var bookJson = JsonConvert.SerializeObject(book);
+            _database.StringSet(GetRedisKey(book.Id), bookJson);
+        }
+
+        public void UpdateBook(Book book)
+        {
+            var existingBook = GetBookById(book.Id);
+            if (existingBook != null)
+            {
+                existingBook.Title = book.Title;
+                existingBook.Author = book.Author;
+
+                var bookJson = JsonConvert.SerializeObject(existingBook);
+                _database.StringSet(GetRedisKey(existingBook.Id), bookJson);
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            _database.KeyDelete(GetRedisKey(id));
+        }
+
+    }
+}
